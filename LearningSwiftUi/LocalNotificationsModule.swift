9c9786922b8ad5b9974e05a7c8436ab40032df6a
Vaibhav Ranga <vaibhavranga2@gmail.com>
2024-07-05 21:17:20 +0530

    added more files

---
+++ LearningSwiftUi/LocalNotificationsModule.swift
@@ -0,0 +1,42 @@
+//
+//  LocalNotificationsModule.swift
+//  LearningSwiftUi
+//
+//  Created by Vaibhav Ranga on 05/07/24.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct LocalNotificationsModule: View {
+    var body: some View {
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
+    }
+}
+
+#Preview {
+    LocalNotificationsModule()
+}
